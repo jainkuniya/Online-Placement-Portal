da4b54b7bd91e26920be310bee7c224c21c5040f
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-10-30 20:23:52 +0530

    Completed recuiter creation by TPO.

--- hello.py
+++ hello.py
@@ -338,7 +338,7 @@ def create_recuiter():
     if 'token' in request.cookies:
         token = request.cookies['token']
         if (free_from_error(verify_token(token))):
-            companyName = request.json['companyName']
+            companyName = request.json['company_name']
             password = get_random_string(5)
             userId = get_random_string(5)
             try:
